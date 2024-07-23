5704b3a3e6f7b4a34b2c9b435c94f4e3766938f2
garcia-lopez <mplg070605@gmail.com>
2024-07-22 22:15:30 -0600

    feat: remove not used function

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ function darkMode() {
     const moon = document.getElementById('celestialBody');
     const body_background = document.getElementById('container');
     const centro = document.getElementById('centro');
-    const animations = applyAnimationBasedOnBreakpoint();
 
     if (moon.classList.contains('sun')) {
         moon.style.animation = 'DarkNight 1s linear';
-        centro.style.animation = animations[0];
+        centro.style.animation = 'DarkNightCenter 0.5s linear';
        body_background.style.backgroundImage = 'radial-gradient(circle at center, #2B124C 60%, #010119)';
         moon.classList.remove('sun');
         centro.classList.remove('small');
@@ -14,27 +13,13 @@ function darkMode() {
         hideClouds();
     } else {
         moon.style.animation = 'SunnyDay 0.5s linear';
-        centro.style.animation = animations[1];
+        centro.style.animation = 'SunnyDayCenter 0.5s linear';
         body_background.style.backgroundImage = 'radial-gradient(circle at center, #f7f5bc 20%, #48CAE4)';
         moon.classList.add('sun');
         centro.classList.remove('center');
         centro.classList.add('small');
         showClouds();
     }
-}
-
-function applyAnimationBasedOnBreakpoint() {
-    const viewportWidth = window.innerWidth;
-    let animations = [];
-
-    if (viewportWidth <= 600){
-        //small screens
-        animations = ['DarkNightCenterSmall 0.5s linear', 'SunnyDayCenterSmall 0.5s linear'];
-    } else {
-        animations = ['DarkNightCenter 0.5s linear', 'SunnyDayCenter 0.5s linear'];
-    }
-
-    return animations;
 }
 
 function showClouds() {
